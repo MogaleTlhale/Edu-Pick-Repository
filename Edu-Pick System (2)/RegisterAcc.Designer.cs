53e4a66ce658212a5b45d192189139b4b7460629
Student <Student@nwu.ac.za>
2022-09-09 16:59:57 +0200

    Added Register Form

---
+++ Edu-Pick System (2)/RegisterAcc.Designer.cs
@@ -0,0 +1,275 @@
+﻿
+namespace Edu_Pick_System__2_
+{
+    partial class RegisterAcc
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.txtName = new System.Windows.Forms.TextBox();
+            this.txtLName = new System.Windows.Forms.TextBox();
+            this.txtEmail = new System.Windows.Forms.TextBox();
+            this.txtNumber = new System.Windows.Forms.TextBox();
+            this.txtPassword = new System.Windows.Forms.TextBox();
+            this.txtCPassword = new System.Windows.Forms.TextBox();
+            this.RBAdmin = new System.Windows.Forms.RadioButton();
+            this.RBStudent = new System.Windows.Forms.RadioButton();
+            this.btnExit = new System.Windows.Forms.Button();
+            this.btnHome = new System.Windows.Forms.Button();
+            this.btnRegister = new System.Windows.Forms.Button();
+            this.SuspendLayout();
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Modern No. 20", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(12, 9);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(115, 22);
+            this.label1.TabIndex = 0;
+            this.label1.Text = "EDU-PICK";
+            this.label1.Click += new System.EventHandler(this.label1_Click);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Modern No. 20", 12F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(12, 51);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(133, 18);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Register Account";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(13, 112);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(60, 13);
+            this.label3.TabIndex = 2;
+            this.label3.Text = "First Name:";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(12, 147);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(61, 13);
+            this.label4.TabIndex = 3;
+            this.label4.Text = "Last Name:";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(12, 188);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(76, 13);
+            this.label5.TabIndex = 4;
+            this.label5.Text = "Email Address:";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(12, 226);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(81, 13);
+            this.label6.TabIndex = 5;
+            this.label6.Text = "Phone Number:";
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(12, 267);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(56, 13);
+            this.label7.TabIndex = 6;
+            this.label7.Text = "Password:";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Location = new System.Drawing.Point(12, 307);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(94, 13);
+            this.label8.TabIndex = 7;
+            this.label8.Text = "Confirm Password:";
+            // 
+            // txtName
+            // 
+            this.txtName.Location = new System.Drawing.Point(125, 109);
+            this.txtName.Name = "txtName";
+            this.txtName.Size = new System.Drawing.Size(120, 20);
+            this.txtName.TabIndex = 8;
+            // 
+            // txtLName
+            // 
+            this.txtLName.Location = new System.Drawing.Point(125, 144);
+            this.txtLName.Name = "txtLName";
+            this.txtLName.Size = new System.Drawing.Size(120, 20);
+            this.txtLName.TabIndex = 9;
+            // 
+            // txtEmail
+            // 
+            this.txtEmail.Location = new System.Drawing.Point(125, 185);
+            this.txtEmail.Name = "txtEmail";
+            this.txtEmail.Size = new System.Drawing.Size(120, 20);
+            this.txtEmail.TabIndex = 10;
+            // 
+            // txtNumber
+            // 
+            this.txtNumber.Location = new System.Drawing.Point(125, 223);
+            this.txtNumber.Name = "txtNumber";
+            this.txtNumber.Size = new System.Drawing.Size(120, 20);
+            this.txtNumber.TabIndex = 11;
+            // 
+            // txtPassword
+            // 
+            this.txtPassword.Location = new System.Drawing.Point(125, 264);
+            this.txtPassword.Name = "txtPassword";
+            this.txtPassword.Size = new System.Drawing.Size(120, 20);
+            this.txtPassword.TabIndex = 12;
+            // 
+            // txtCPassword
+            // 
+            this.txtCPassword.Location = new System.Drawing.Point(125, 304);
+            this.txtCPassword.Name = "txtCPassword";
+            this.txtCPassword.Size = new System.Drawing.Size(120, 20);
+            this.txtCPassword.TabIndex = 13;
+            // 
+            // RBAdmin
+            // 
+            this.RBAdmin.AutoSize = true;
+            this.RBAdmin.Location = new System.Drawing.Point(38, 354);
+            this.RBAdmin.Name = "RBAdmin";
+            this.RBAdmin.Size = new System.Drawing.Size(54, 17);
+            this.RBAdmin.TabIndex = 14;
+            this.RBAdmin.TabStop = true;
+            this.RBAdmin.Text = "Admin";
+            this.RBAdmin.UseVisualStyleBackColor = true;
+            this.RBAdmin.CheckedChanged += new System.EventHandler(this.RBAdmin_CheckedChanged);
+            // 
+            // RBStudent
+            // 
+            this.RBStudent.AutoSize = true;
+            this.RBStudent.Location = new System.Drawing.Point(173, 354);
+            this.RBStudent.Name = "RBStudent";
+            this.RBStudent.Size = new System.Drawing.Size(62, 17);
+            this.RBStudent.TabIndex = 15;
+            this.RBStudent.TabStop = true;
+            this.RBStudent.Text = "Student";
+            this.RBStudent.UseVisualStyleBackColor = true;
+            this.RBStudent.CheckedChanged += new System.EventHandler(this.RBStudent_CheckedChanged);
+            // 
+            // btnExit
+            // 
+            this.btnExit.Location = new System.Drawing.Point(31, 401);
+            this.btnExit.Name = "btnExit";
+            this.btnExit.Size = new System.Drawing.Size(75, 23);
+            this.btnExit.TabIndex = 16;
+            this.btnExit.Text = "Exit";
+            this.btnExit.UseVisualStyleBackColor = true;
+            // 
+            // btnHome
+            // 
+            this.btnHome.Location = new System.Drawing.Point(170, 401);
+            this.btnHome.Name = "btnHome";
+            this.btnHome.Size = new System.Drawing.Size(75, 23);
+            this.btnHome.TabIndex = 17;
+            this.btnHome.Text = "Home";
+            this.btnHome.UseVisualStyleBackColor = true;
+            // 
+            // btnRegister
+            // 
+            this.btnRegister.Location = new System.Drawing.Point(320, 401);
+            this.btnRegister.Name = "btnRegister";
+            this.btnRegister.Size = new System.Drawing.Size(75, 23);
+            this.btnRegister.TabIndex = 18;
+            this.btnRegister.Text = "Register";
+            this.btnRegister.UseVisualStyleBackColor = true;
+            this.btnRegister.Click += new System.EventHandler(this.btnRegister_Click);
+            // 
+            // RegisterAcc
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.btnRegister);
+            this.Controls.Add(this.btnHome);
+            this.Controls.Add(this.btnExit);
+            this.Controls.Add(this.RBStudent);
+            this.Controls.Add(this.RBAdmin);
+            this.Controls.Add(this.txtCPassword);
+            this.Controls.Add(this.txtPassword);
+            this.Controls.Add(this.txtNumber);
+            this.Controls.Add(this.txtEmail);
+            this.Controls.Add(this.txtLName);
+            this.Controls.Add(this.txtName);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Name = "RegisterAcc";
+            this.Text = "RegisterAcc";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.TextBox txtName;
+        private System.Windows.Forms.TextBox txtLName;
+        private System.Windows.Forms.TextBox txtEmail;
+        private System.Windows.Forms.TextBox txtNumber;
+        private System.Windows.Forms.TextBox txtPassword;
+        private System.Windows.Forms.TextBox txtCPassword;
+        private System.Windows.Forms.RadioButton RBAdmin;
+        private System.Windows.Forms.RadioButton RBStudent;
+        private System.Windows.Forms.Button btnExit;
+        private System.Windows.Forms.Button btnHome;
+        private System.Windows.Forms.Button btnRegister;
+    }
+}
---
+++ Edu-Pick System (2)/RegisterAcc.cs
@@ -0,0 +1,83 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.SqlClient;
+
+namespace Edu_Pick_System__2_
+{
+    public partial class RegisterAcc : Form
+    {
+        public RegisterAcc()
+        {
+            InitializeComponent();
+        }
+        SqlConnection conn = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C: \Users\Student\Source\Repos\MogaleTlhale\Edu - Pick - Repository\Edu - Pick System(2)\DBAdmin.mdf;Integrated Security=True");
+        SqlConnection conn2 = new SqlConnection(@"Data Source=(LocalDB)\MSSQLLocalDB;AttachDbFilename=C: \Users\Student\Source\Repos\MogaleTlhale\Edu - Pick - Repository\Edu - Pick System(2)\DBStudent.mdf;Integrated Security=True");
+        SqlDataAdapter adapter = new SqlDataAdapter();
+       
+
+        
+        private void label1_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RBAdmin_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void RBStudent_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void btnRegister_Click(object sender, EventArgs e)
+        {
+            string FName, LName, Email, PhoneNum, Password, CPassword;
+            FName = txtName.Text;
+            LName = txtLName.Text;
+            Email = txtEmail.Text;
+            PhoneNum = txtNumber.Text;
+            Password = txtPassword.Text;
+            CPassword = txtCPassword.Text;
+            try
+            {
+                conn2.Open();
+                string sql_insert = "Insert Into StudentTable Values('" + FName + "','" +
+                LName + "','" + Email + "','" + PhoneNum + "','" + Password + "','" + CPassword + "')";
+                SqlCommand cmd = new SqlCommand(sql_insert, conn2);
+
+                
+                adapter = new SqlDataAdapter();
+
+
+                adapter.InsertCommand = cmd;
+
+                cmd.ExecuteNonQuery();
+                MessageBox.Show("Data successfully inserted");
+                conn2.Close();
+            }
+            catch (SqlException ex)
+            {
+                MessageBox.Show(ex.Message);
+            }
+            finally
+            {
+                txtName.Clear();
+                txtLName.Clear();
+                txtEmail.Clear();
+                txtNumber.Clear();
+                txtPassword.Clear();
+                txtCPassword.Clear();
+                
+            }
+        }
+    }
+}
